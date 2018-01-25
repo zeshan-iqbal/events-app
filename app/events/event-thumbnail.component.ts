e83440efa9538e5e7cdaf8051f387fbd73258451
zeeshan.waterway <zeeshan.waterway@nxvt.com>
2018-01-25 21:06:30 +0500

    *ngFor structural directive

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -9,19 +9,18 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
         <div>Time: {{event.time}}</div>
         <div>Price: \${{event.price}}</div>
         <div>
-            <span>Location: {{event.location.address}}</span>
-            <span>&nbsp;</span>
-            <span>{{event.location.city}}, {{event.location.country}}</span>
+            <span>Location: {{event.location.address}}</span>            
+            <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
         </div>
-        <button class="btn btn-primary" (click)="handleClickMe()">Click Me!</button>
     </div>
-    `
+    `,
+    styles: [
+        `.thumbnail { min-height: 210px; }
+        .pad-left {padding-left: 10px;}
+        .well div {color: #bbb;}
+        `
+    ]
 })
 export class EventThumbnailComponent {
     @Input() event: any;
-    @Output() eventClick = new EventEmitter()
-
-    handleClickMe(){
-        this.eventClick.emit(this.event.name);
-    }
 }
