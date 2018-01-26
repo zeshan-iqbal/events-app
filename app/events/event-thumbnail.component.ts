5fe308596b9ac4130237b24be1b49e5b194d26b5
zeeshan.waterway <zeeshan.waterway@nxvt.com>
2018-01-26 19:01:34 +0500

    ngSwitch directive

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,13 +4,18 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
     selector: 'event-thumbnail',
     template: `
     <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
+        <h2>{{event?.name}}</h2>
+        <div>Date: {{event?.date}}</div>
+        <div [ngSwitch]="event?.time">
+            Time: {{event?.time}}
+            <span class="pad-left" *ngSwitchCase="'8:00 am'">(Early Start)</span>
+            <span class="pad-left" *ngSwitchCase="'10:00 am'">(Late Start)</span>
+            <span class="pad-left" *ngSwitchDefault>(Normal Start)</span>
+        </div>
+        <div>Price: \${{event?.price}}</div>
         <div>
-            <span>Location: {{event.location.address}}</span>            
-            <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+            <span>Location: {{event?.location?.address}}</span>            
+            <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
         </div>
     </div>
     `,
