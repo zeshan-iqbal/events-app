13600548bdf69b831b092ca93511924390eb5fe5
Zeeshan Iqbal <link2zeshan@gmail.com>
2018-01-26 22:55:14 +0500

    Services and DI

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,10 +6,12 @@ import { EventListComponent } from "./events/events-list.component";
 import { EventThumbnailComponent } from "./events/event-thumbnail.component";
 import { Input } from "@angular/core/src/metadata/directives";
 import { NavComponent } from "./nav/nav.component";
+import { EventsService } from "./events/shared/EventsService";
 
 @NgModule({
     imports: [BrowserModule],
     declarations: [EventsAppComponenet, EventListComponent, EventThumbnailComponent, NavComponent],
+    providers: [EventsService],
     bootstrap: [EventsAppComponenet]
 })
 export class AppModule {
