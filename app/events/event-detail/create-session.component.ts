da9184ac95125a8d9d461129d7fe13ed2b5edb0c
zeeshan.waterway <zeeshan.waterway@nxvt.com>
2018-02-02 18:48:38 +0500

    Reactive Form: Complete create new session form and validation

--- app/events/event-detail/create-session.component.ts
+++ app/events/event-detail/create-session.component.ts
@@ -1,35 +1,56 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { ISession } from "../index";
 
 
 @Component({
-    templateUrl: './app/events/event-detail/create-session.component.html'
+    templateUrl: './app/events/event-detail/create-session.component.html',
+    styles: [`
+  em { float: right; color: #E05C65; padding-left: 10 px;}
+  .error input {background-color: #E3C3C5;}
+  .error ::-webkit-input-placeholder {color: #9999;}
+  .error ::-moz-placeholder {color: #9999;}
+  .error :-moz-placeholder {color: #9999;}
+  .error :-ms-input-placeholder {color: #9999;}
+  `]
 })
 export class CreateSessionComponent implements OnInit{
-    private createSessionForm: FormGroup;
-    private abstract: FormControl;
-    private level: FormControl;
-    private duration: FormControl;
-    private presenter: FormControl;
-    private name: FormControl;
-    private voters: FormControl;
+    newSessionForm: FormGroup;
+    abstract: FormControl;
+    level: FormControl;
+    duration: FormControl;
+    presenter: FormControl;
+    name: FormControl;
+    //private voters: FormControl;
 
     ngOnInit(): void {
         this.name = new FormControl("", Validators.required);
         this.presenter = new FormControl("", Validators.required);
         this.duration = new FormControl("", Validators.required);
         this.level = new FormControl("", Validators.required);
-        this.abstract = new FormControl("", [Validators.required, Validators.maxLength(400)]);
-        this.voters = new FormControl("", Validators.required);
+        this.abstract = new FormControl("", [Validators.required, Validators.maxLength(400)]);        
 
-        this.createSessionForm = new FormGroup({
+        this.newSessionForm = new FormGroup({
             name: this.name,
             presenter: this.presenter,
             duration: this.duration,
             level: this.level,
-            abstract: this.abstract,
-            voters: this.voters
+            abstract: this.abstract            
         });
+    }
+
+    saveSession(formValues){
+
+        let session: ISession = {
+            id: undefined,
+            name: formValues.name,
+            duration: +formValues.duration,
+            abstract: formValues.abstract,
+            level: formValues.level,
+            presenter: formValues.presenter,
+            voters: []
+        }
+        console.log(session);
     }
 
 }
