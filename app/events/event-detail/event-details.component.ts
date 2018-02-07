4aea7b2b915b4f72f849a2c96ca8c79f8d3409d4
zeeshan.waterway <zeeshan.waterway@nxvt.com>
2018-02-07 15:00:55 +0500

    Implement sorting

--- app/events/event-detail/event-details.component.ts
+++ app/events/event-detail/event-details.component.ts
@@ -16,6 +16,7 @@ export class EventDetailsComponent implements OnInit {
     event: IEvent;
     addMode: boolean
     filterBy: string = 'all'
+    sortBy: string = 'votes'
     constructor(private eventsService: EventsService, private route: ActivatedRoute) {
     }
 
--- app/events/event-detail/session-list.component.ts
+++ app/events/event-detail/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { ISession } from "../index";
 
 
@@ -6,19 +6,37 @@ import { ISession } from "../index";
     selector: 'session-list',
     templateUrl: './app/events/event-detail/session-list.component.html'
 })
-export class SessionListComponent implements OnChanges {        
+export class SessionListComponent {
+    private _sortBy: string;
+    private _filterBy: string;
+
     @Input() sessions: ISession[]
-    @Input() filterBy: string
+    @Input()
+    set filterBy(value: string) {
+        if (this.sessions) this.filterSessions(value);
+        this._filterBy = value;
+    }
+    @Input()
+    set sortBy(value: string) {
+        value == 'name' ? this.visibleSessions.sort(sortByNameAsc) : this.visibleSessions.sort(sortByVotesDsc)
+        this._sortBy = value
+    }
     visibleSessions: ISession[]
 
-    ngOnChanges() {
-        if(this.sessions) this.filterSessions(this.filterBy);           
-    }
-    
     filterSessions(filterBy: string) {
-        if(filterBy == 'all')
+        if (filterBy == 'all')
             this.visibleSessions = this.sessions.slice(0);
         else
             this.visibleSessions = this.sessions.filter(s => s.level.toLocaleLowerCase() === filterBy)
     }
+}
+
+function sortByNameAsc(s1: ISession, s2: ISession): number {
+    if (s1.name > s2.name) return 1
+    else if (s1.name === s2.name) return 0
+    else return -1
+}
+
+function sortByVotesDsc(s1: ISession, s2: ISession): number {
+    return s2.voters.length - s1.voters.length
 }
